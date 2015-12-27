d5a2e666dc6d0340a24543deb5e6c93a735e0c8f
Richard Paez <richardp809@gmail.com>
2015-12-27 01:38:55 -0500

    Initial commit added files

---
+++ app.js
@@ -0,0 +1,17 @@
+var main = function(){
+//   var navbar = $("#topbar");
+//   $(window).scroll(function(){
+//     if(document.body.scrollTop != 0){
+//       //make the navbar transparent
+//       navbar.removeClass("topbar");
+//       navbar.addClass("transbar");
+//     }
+//     else if (document.body.scrollTop === 0) {
+//       //revert transparency
+//       navbar.removeClass("transbar");
+//       navbar.addClass("topbar");
+//     }
+//   });
+}
+
+$(document).ready(main);
